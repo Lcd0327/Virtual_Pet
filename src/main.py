30e5b9e27cc900ffab3d96846398e9e5299bb21a
Lcd0327 <lcd0327@gmail.com>
2025-04-22 10:33:36 +0800

    put an image on window

--- src/main.py
+++ src/main.py
@@ -1,12 +1,19 @@
 from ursina import *
+import os
+from pathlib import Path
 from pet import VirtualPet
 from control import  movement
 
-app = Ursina()
+application.asset_folder = Path(os.path.abspath(os.path.join(os.path.dirname(__file__), '../assets')))
+print(application.asset_folder)
 
+app = Ursina()
+#image_entity = Entity(model='quad',texture=('IMG_5426.png'),  position=(0.5, -0.5))
 pet = VirtualPet()
+
+print(os.path)
 
 def update():
     movement(pet, held_keys)
-    
+#     pass   
 app.run()
--- src/pet.py
+++ src/pet.py
@@ -3,9 +3,9 @@ import os
 
 class VirtualPet(Entity):
     def __init__(self):
+        print(f"load model from: {os.path}")
         super().__init__(
             model='cube',
-            color=color.white,
-            scale=(1, 1, 1),
-            position=(0,0,0)
+            texture=(f'{application.asset_folder}/IMG_5426.png'), 
+            position=(0.5, -0.5),
         )
